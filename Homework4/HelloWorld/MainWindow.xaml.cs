c7f6b749e4da2eac689a9f311e88e30c9a7cdb32
Nell <nbrisard@coxautoinc.com>
2020-05-29 23:42:13 -0500

    Adding homework assignment 4

---
+++ Homework4/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            uxSubmit.IsEnabled = false;
+        }
+
+
+        public void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (IsUSOrCanadianZipCode(uxTextBox.Text))
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else 
+            {
+                uxSubmit.IsEnabled = false;
+            }
+        }
+
+
+        private bool IsUSOrCanadianZipCode(string zipCode)
+        {
+            var _usZipRegEx = @"^\d{5}(?:[-\s]\d{4})?$";
+            var _caZipRegEx = @"^([ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9])$";
+            var validZipCode = true;
+            if ((!Regex.Match(zipCode, _usZipRegEx).Success) && (!Regex.Match(zipCode, _caZipRegEx).Success))
+            {
+                validZipCode = false;
+            }
+            return validZipCode;
+        }
+    }
+
+}
---
+++ Homework4/HelloWorld/Models/User.cs
@@ -0,0 +1,146 @@
+﻿using System.ComponentModel;
+
+namespace HelloWorld.Models
+{
+    class User : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string name = string.Empty;
+        private string password = string.Empty;
+        private string nameError;
+
+        // Add ToString method
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                    OnPropertyChanged("NameError");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        // IDataErrorInfo interface
+        public string Error
+        {
+            get
+            {
+                return NameError;
+            }
+        }
+
+        // IDataErrorInfo interface
+        // Called when ValidatesOnDataErrors=True
+        public string this[string columnName]
+        {
+            get
+            {
+                NameError = "";
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            if (string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Name cannot be empty.";
+                            }
+                            if (Name.Length > 12)
+                            {
+                                NameError = "Name cannot be longer than 12 characters.";
+                            }
+                            break;
+                        }
+                }
+                return NameError;
+            }
+        }
+
+        // INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
+public interface IDataErrorInfo
+{
+    //
+    // Summary:
+    //     Gets the error message for the property with the given name.
+    //
+    // Parameters:
+    //   columnName:
+    //     The name of the property whose error message to get.
+    //
+    // Returns:
+    //     The error message for the property. The default is an empty string ("").
+    string this[string columnName] { get; }
+
+    //
+    // Summary:
+    //     Gets an error message indicating what is wrong with this object.
+    //
+    // Returns:
+    //     An error message indicating what is wrong with this object. The default is an
+    //     empty string ("").
+    string Error { get; }
+}
+
+
+// Summary:
+//     Notifies clients that a property value has changed.
+public interface INotifyPropertyChanged
+{
+    //
+    // Summary:
+    //     Occurs when a property value changes.
+    event PropertyChangedEventHandler PropertyChanged;
+}
